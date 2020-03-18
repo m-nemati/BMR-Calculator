1ca5722264b25939ca674e36f9bffebfc50a805f
m-nemati <m.nemati2002@gmail.com>
2020-03-18 15:39:46 +0330

    v = 03

--- app/src/main/java/ir/mnemati/calcbmr/MainActivity.kt
+++ app/src/main/java/ir/mnemati/calcbmr/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,35 @@ class MainActivity : AppCompatActivity() {
         // Set Title
         title = "محاسبگر شاخص BMR"
 
+        /* ***** Calculate the BMR ***** */
+        btnCalcBMR.setOnClickListener {
+
+            if (edtWeigh.text.toString()== "" || edtHeight.text.toString() == "" || edtAge.text.toString() == "") {
+                Toast.makeText(this, "لطفا مقادیر وزن، قد و سن خود را وارد نمایید.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            try {
+                // Save input data to variables
+                val weight: Double = edtWeigh.text.toString().toDouble()
+                val height: Double = edtHeight.text.toString().toDouble() / 100
+                val age: Double = edtAge.text.toString().toDouble()
+
+                if (rbMen.isChecked) {
+
+                }
+                else if (rbWomen.isChecked) {
+
+                }
+                else {
+                    Toast.makeText(this, "لطفا جنسیت خود را تعیین کنید", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            catch (ex: Exception) {
+                Toast.makeText(this, "مقادیر وزن، قد و سن را درست وارد نمایید.", Toast.LENGTH_SHORT).show()
+            }
+        }
         /* ***** Go to BMR Explain Activity ***** */
         btnAboutBMR.setOnClickListener {
 
